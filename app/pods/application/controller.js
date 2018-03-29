0c07508e609e565df38b74d2e21f359550da2e56
Argir Argirov <dj.giro@gmail.com>
2018-03-29 17:22:15 -0400

    Show page divs done, and console log action on all buttons

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -6,5 +6,10 @@ export default Ember.Controller.extend({
     isHiddenNavRoute: Ember.computed('currentRouteName', function(){
       //return true if current route is one of the non nav routes
       return this.get('hideNavRoutes').indexOf(this.get('currentRouteName')) !== -1;
-    })
+    }),
+    actions: {
+      click() {
+        console.log('clicked');
+      }
+    }
 });
