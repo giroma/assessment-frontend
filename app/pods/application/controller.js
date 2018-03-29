d437f73387454286154cedc3be8f5d710e16ccbd
Argir Argirov <dj.giro@gmail.com>
2018-03-29 14:31:40 -0400

    hide nav on show page

---
+++ app/pods/application/controller.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+//functions so that app template NAV bar isnt shown on some pages
+export default Ember.Controller.extend({
+   hideNavRoutes: ['products.show'],
+    isHiddenNavRoute: Ember.computed('currentRouteName', function(){
+      //return true if current route is one of the non nav routes
+      return this.get('hideNavRoutes').indexOf(this.get('currentRouteName')) !== -1;
+    })
+});
